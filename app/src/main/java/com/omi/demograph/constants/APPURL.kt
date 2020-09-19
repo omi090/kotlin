01a437d6ae760c7299bb88360ec9bb411b7f5b8e
omi <dev@enthralltech.com>
2020-09-20 00:12:32 +0530

    Added base URL and some basic stuff

--- app/src/main/java/com/omi/demograph/constants/APPURL.kt
+++ app/src/main/java/com/omi/demograph/constants/APPURL.kt
@@ -1,7 +1,6 @@
 package com.empoweredtls.constants
 
 object APPURL {
-    const val PUBLIC_KEY_HASH = "sha256/Ko8tivDrEjiY90yGasP6ZpBU4jwXvHqVvQI0GS3GNdA="
-
+    const val BASE_URL = "https://plobalapps.s3-ap-southeast-1.amazonaws.com/"
 
 }
