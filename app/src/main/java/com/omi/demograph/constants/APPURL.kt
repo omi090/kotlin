7dc7fd4d481e649611f0045b2d9b9f68e18771b5
omi090 <61613774+omi090@users.noreply.github.com>
2020-09-19 23:59:57 +0530

    Initial commit

---
+++ app/src/main/java/com/omi/demograph/constants/APPURL.kt
@@ -0,0 +1,7 @@
+package com.empoweredtls.constants
+
+object APPURL {
+    const val PUBLIC_KEY_HASH = "sha256/Ko8tivDrEjiY90yGasP6ZpBU4jwXvHqVvQI0GS3GNdA="
+
+
+}
---
+++ app/src/main/java/com/omi/demograph/constants/AppContants.kt
@@ -0,0 +1,12 @@
+package com.empoweredtls.constants
+
+object AppContants {
+
+    const val VIEW_TYPE_ITEM = 0
+    const val VIEW_TYPE_LOADING = 1
+    const val FILTER_INTENT = 100
+    const val FILTER_INTENT_RESULT = 101
+    const val SCREEN_ASSIGNED = "ASSIGNED"
+    const val SCREEN_IN_PROGRESS = "IN_PROGRESS"
+    const val SCREEN_CATALOG = "CATALOG"
+}
